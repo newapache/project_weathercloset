65700772056846711acdbb6fcf8e2d9c2815873f
leeminhyeon <leeminhyun0613@gmail.com>
2017-08-11 06:44:42 +0000

    weather page -ing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,20 @@ class HomeController < ApplicationController
         @data = forecast.daily.data
         #unixTime = forecast.hourly.data[0].time.to_s
         #@localTime = DateTime.strptime(unixTime,'%s')
-        @korTime = Time.now.in_time_zone("Seoul").hour
+        
+        
+        @hourlyData = forecast.hourly.data
+    end
+    
+    def weather
+        forecast = forecast = ForecastIO.forecast(37.56, 126.97, params: { units: 'si' })
+        @currentData = forecast.currently
+        @timezone = forecast.timezone
+        #@daily = forecast.daily.summary
+        @data = forecast.daily.data
+        #unixTime = forecast.hourly.data[0].time.to_s
+        #@localTime = DateTime.strptime(unixTime,'%s')
+        
         
         @hourlyData = forecast.hourly.data
     end
