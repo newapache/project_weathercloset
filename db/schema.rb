69efc339ce55e1ff9953f2ae55d77bcf226c375a
Yon Kim <jinny309@gmail.com>
2017-08-16 14:49:29 +0000

    백 컨트롤러 홈에서 날씨로 옷 정보 제공 추가

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813055659) do
+ActiveRecord::Schema.define(version: 20170813055310) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -27,13 +27,6 @@ ActiveRecord::Schema.define(version: 20170813055659) do
     t.integer  "likes_count"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "logins", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
