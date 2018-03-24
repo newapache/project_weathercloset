b0ccbeea8067603ef2109db573505b913c88f951
Sunkyoung <sun_k109@naver.com>
2018-03-24 13:43:32 +0000

    try..

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,13 @@
 require 'carrierwave/orm/activerecord'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
   CarrierWave.configure do |config|
-  
     config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id: ENV["CLOSET_AWS_KEY"],
-      aws_secret_access_key: ENV["CLOSET_AWS_SECRET"],
-      region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
+      :provider              =>'AWS',                        # required
+      :aws_access_key_id =>ENV['CLOSET_AWS_KEY'],
+      :aws_secret_access_key =>ENV["CLOSET_AWS_SECRET"],
+      :region                =>'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'weathercloset-dev'                          # required
   end
-end
