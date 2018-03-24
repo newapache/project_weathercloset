69ee417932de6b5f8466c72d4b57b8fd8238d1be
Sunkyoung <sun_k109@naver.com>
2018-03-24 12:56:14 +0000

    what's wrong..

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,6 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    # aws_access_key_id:     "#{ENV['AWS_KEY']}",                     # required
-    # aws_secret_access_key: "#{ENV['AWS_SECRET']}",                         # required
     aws_access_key_id: ENV["CLOSET_AWS_KEY"],
     aws_secret_access_key: ENV["CLOSET_AWS_SECRET"],
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
